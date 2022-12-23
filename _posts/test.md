26f2e6d106ba388bc5ef6f0128e9b97c6a7570c2
samuzora <lucastanyj@gmail.com>
2022-12-23 09:29:47 +0800

    post: pretty hyper phrog writeup

--- _posts/test.md
+++
@@ -1,8 +0,0 @@
----
-title: Hello world
-date: 21 Dec 2022
----
-
-# Hello
-
-hello world!
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,8 +1,10 @@
-import Markdown from "markdown-to-jsx"
 import { GetStaticProps } from "next"
 import { ParsedUrlQuery } from "querystring"
+import { ReactMarkdown } from "react-markdown/lib/react-markdown"
 import Container from "../../components/Container"
 import { getPost, getSlugs, Post } from "../../server/posts"
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { coldarkDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 
 interface Props {
   post: Post
@@ -18,7 +20,26 @@ export default (props: Props) => {
       <h1>{props.post.title}</h1>
       <h5>{props.post.date}</h5>
       <hr />
-      <Markdown>{props.post.content}</Markdown>
+      <ReactMarkdown
+        children={props.post.content}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || '')
+            return !inline && match ? (
+              <SyntaxHighlighter
+                children={String(children).replace(/\n$/, '')}
+                style={coldarkDark}
+                language={match[1]}
+                PreTag="div"
+              // {...props}
+              />
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            )
+          }
+        }} />
     </Container>
   )
 }
