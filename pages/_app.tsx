c5b9a617425d88ced3fce07f0dfab4f280b6aac8
samuzora <lucastanyj@gmail.com>
2023-07-18 09:12:02 +0800

    Add social media

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,13 @@ import Navbar from '../components/Navbar'
 import { useRouter } from 'next/router'
 import Loading from '../components/Loading'
 import Container from '../components/Container'
+import Link from 'next/link'
+
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+import styles from "../styles/app.module.scss"
 
 const font = IBM_Plex_Sans({
   weight: '300',
@@ -31,6 +38,17 @@ export default function App({ Component, pageProps }: AppProps) {
         <div className="background" />
         <Navbar />
         <Component {...pageProps} />
+        <div className={styles.footer}>
+          <Link href="https://twitter.com/_samuzora">
+            <TwitterIcon />
+          </Link>
+          <Link href="https://www.instagram.com/samu_zora/">
+            <InstagramIcon />
+          </Link>
+          <Link href="https://github.com/samuzora">
+            <GitHubIcon />
+          </Link>
+        </div>
       </main>
     )
   }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,6 @@ import Head from 'next/head'
 import { join } from 'path'
 import Container from '../components/Container'
 import styles from '../styles/Home.module.scss'
-import Link from 'next/link'
-
-import TwitterIcon from '@mui/icons-material/Twitter';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import AccessibleIcon from '@mui/icons-material/Accessible';
 
 interface Props {
   content: string
@@ -32,17 +26,6 @@ export default (props: Props) => {
           samuzora
         </div>
         <Markdown className={styles.content}>{props.content}</Markdown>
-        <div className={styles.footer}>
-          <Link href="https://twitter.com/_samuzora">
-            <TwitterIcon />
-          </Link>
-          <Link href="https://www.instagram.com/samu_zora/">
-            <InstagramIcon />
-          </Link>
-          <Link href="https://github.com/samuzora">
-            <GitHubIcon />
-          </Link>
-        </div>
       </Container>
     </div>
   )
