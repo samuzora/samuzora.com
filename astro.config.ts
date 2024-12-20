78c39bfcf200b7c54d4069cdda1410193cb4d17e
samuzora <lucastanyj@gmail.com>
2024-12-20 22:41:25 +0800

    add new styling for code blocks

---
+++ astro.config.ts
@@ -0,0 +1,49 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import yaml from "@rollup/plugin-yaml"
+import { remarkReadingTime } from "./remark-reading-time.mjs";
+import sectionize from "remark-sectionize";
+import { rehypePrettyCode } from "rehype-pretty-code";
+import { 
+  transformerNotationDiff, 
+  transformerNotationFocus,
+  transformerMetaHighlight,
+} from "@shikijs/transformers";
+
+import vue from "@astrojs/vue";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://samuzora.com",
+  integrations: [vue(), tailwind(), sitemap()],
+
+  markdown: {
+    syntaxHighlight: false,
+    shikiConfig: {
+      wrap: true
+    },
+    rehypePlugins: [
+      [
+        rehypePrettyCode,
+        {
+          theme: "catppuccin-mocha",
+          transformers: [
+            transformerNotationDiff(),
+            transformerNotationFocus(),
+            transformerMetaHighlight(),
+          ],
+        },
+      ],
+    ],
+    remarkPlugins: [
+      remarkReadingTime,
+      sectionize as any,
+    ],
+  },
+
+  vite: {
+    plugins: [yaml()]
+  },
+});
