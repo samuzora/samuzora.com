7e8cca19218763fd7858cf67d86c1c079acb0555
samuzora <lucastanyj@gmail.com>
2025-01-17 18:54:45 +0800

    convert png to webp

--- src/components/PostHeader.vue
+++ src/components/PostHeader.vue
@@ -9,7 +9,7 @@ const props = defineProps(["post", "remarkPluginFrontmatter"]);
   <div>
     <div class="flex gap-3">
       <div>
-        <img class="rounded-xl aspect-square w-14 border border-[--secondary-bg]" src="/assets/samuzora.png"
+        <img class="rounded-xl aspect-square w-14 border border-[--secondary-bg]" src="/assets/samuzora.webp"
           alt="author" />
       </div>
       <div class="flex flex-col justify-around">
