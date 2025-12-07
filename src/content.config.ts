aa1944e7cfab25002e107afdfebfc98fe844642d
samuzora <lucastanyj@gmail.com>
2025-12-07 22:38:53 +0800

    add hiding drafts

--- src/content.config.ts
+++ src/content.config.ts
@@ -9,7 +9,8 @@ const blog = defineCollection({
     date: z.coerce.date(),
     excerpt: z.string(),
     category: z.string(), // should be a normalized-string, no caps, spaces, or slashes
-    tags: z.array(z.string())
+    tags: z.array(z.string()),
+    draft: z.boolean().optional()
   })
 });
 
